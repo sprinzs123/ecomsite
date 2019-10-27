d29daefff626dc06046611938bfec83556593b2a
sprinzs123 <sprinzs351@gmail.com>
2019-10-26 21:05:43 -0500

    second

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop',
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ecomsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ecomsite/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
